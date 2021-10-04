758625be872514e0d96a54cd49a858cb1f347364
Agustin <jagustinbassi@gmail.com>
2021-10-04 18:06:03 -0300

    Adds user authentication and permissions to views. Changes Snippet model.

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,8 +1,20 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
+
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 
 class SnippetSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    
     class Meta:
         model = Snippet
-        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
+        fields = ['id', 'title', 'code', 'linenos', 'language', 'style', 'owner']
+
+
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'snippets']
--- snippets/urls.py
+++ snippets/urls.py
@@ -8,6 +8,8 @@ app_name = 'snippets'
 urlpatterns = [
     path('', views.SnippetList.as_view(), name='snippets'),
     path('<int:pk>/', views.SnippetDetail.as_view(), name='snippet_detail'),
+    path('users/', views.UserList.as_view(), name='users'),
+    path('users/<int:pk>/', views.UserDetail.as_view(), name="user_detail"),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)

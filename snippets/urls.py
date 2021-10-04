aaa3f4022839e041a9d1186cb7210aa9fa7ef9fc
Agustin <jagustinbassi@gmail.com>
2021-10-04 13:54:09 -0300

    Improves func based views and apply formats to endpoints

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+
 from snippets import views
 
 app_name = 'snippets'
@@ -7,3 +9,5 @@ urlpatterns = [
     path('', views.snippet_list),
     path('<int:pk>/', views.snippet_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
